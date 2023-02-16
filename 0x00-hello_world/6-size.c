d2b211b8f70eb2ddcb3a9d8d39ca40265215d55f
mutizep <prosperitymutizerayz@gmail.com>
2023-02-16 06:04:51 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("size of a char: %lu byte(s)\n", (unassigned long)sizeof(a));
-printf("size of a int: %lu byte(s)\n", (unassigned long)sizeof(b));
-printf("size of a long int: %lu byte(s)\n", (unassigned long)sizeof(c));
-printf("size of a long long int: %lu byte(s)\n", (unassigned long)sizeof(d));
-printf("size of a float: %lu byte(s)\n", (unassigned long)sizeof(f));
+printf("Size of a char: %zu byte(s)\n", sizeof(a));
+printf("Size of a int: %zu  byte(s)\n", sizeof(b));
+printf("Size of a long int: %zu byte(s)\n", (sizeof(c));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+printf("Size of a float: %zu byte(s)\n", sizeof(f));
 return (0);
 }
