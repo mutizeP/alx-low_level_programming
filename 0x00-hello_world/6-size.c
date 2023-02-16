cbfdfe37f8491583d4a9316031edbfa59d64bf4f
mutizep <prosperitymutizerayz@gmail.com>
2023-02-16 06:09:32 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
 * Return: 0 (Success)
 */
 int main(void)
-
+{
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of a int: %zu  byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", (sizeof(long int));
